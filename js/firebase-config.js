3e0c3e7efbed754fe51019dac45ab429ccd23324
psptskasaofficials <psptskasa.officials@gmail.com>
2026-09-26 00:22:40 +0700

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtSTgrBWY5aDahE6hq_yr4oSxpxl3QtcQ",
+  authDomain: "skawan-tv.firebaseapp.com",
+  databaseURL: "https://skawan-tv-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "skawan-tv",
+  storageBucket: "skawan-tv.firebasestorage.app",
+  messagingSenderId: "501619282826",
+  appId: "1:501619282826:web:81e624ac35e9798062ae73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
